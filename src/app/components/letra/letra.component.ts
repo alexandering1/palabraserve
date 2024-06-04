8a1a388dde9c24c764233342f43892fd44edbcbe
alexandering1 <cueromorenojefer@gmail.com>
2024-06-04 18:39:02 -0500

    Descripción de los cambios que realizaste

--- src/app/components/letra/letra.component.ts
+++ src/app/components/letra/letra.component.ts
@@ -12,7 +12,7 @@ export class LetraComponent implements OnInit {
   @Input() disabled: boolean = true;  // Nuevo input para manejar el estado deshabilitado
   public flip: boolean = false;
   public opcion: string = '';
-  public css: string = ''; // 1. igual Verde. 2. naranja. 3. gris
+  public css: string = ''; // 1. igual Verde. 2. naranja. 3. gris  ....
   @Output() opcionCambiada = new EventEmitter<void>();
 
   constructor() { }
